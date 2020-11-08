17bc7a82c82feba441395d4a9e40cca7dbadc309
Diogo Dores <diogo.dores@gmail.com>
2020-11-08 17:40:06 +0000

    Created banner

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,43 @@
 <template>
   <div id="app">
     <Banner></Banner>
+    <About></About>
+    <Experience></Experience>
+    <Projects></Projects>
+    <Skills></Skills>
+    <Contacts></Contacts>
   </div>
 </template>
 
 <script>
 import Banner from './components/Banner.vue'
+import About from './components/About.vue'
+import Experience from './components/Experience.vue'
+import Projects from './components/Projects.vue'
+import Skills from './components/Skills.vue'
+import Contacts from './components/Contacts.vue'
 
 export default {
   name: 'App',
   components: {
-    Banner
+    Banner,
+    About,
+    Experience,
+    Projects,
+    Skills,
+    Contacts
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: $font-title;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  body{
+    margin: 0;
+  }
 
-}
+  #app {
+    font-family: $font-title;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 </style>
--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="hello">
-    yoyoyoyoyoy
+  <div id="banner">
+    <div id="navbar">
+      <ul>
+        <li>About Me</li>
+        <li>Experience</li>
+        <li>Projects</li>
+        <li>Skills</li>
+        <li>Contacts</li>
+      </ul>
+    </div>
+
+    <div id="welcome">
+      <p>Welcome</p>
+      <p class="name">I'm Diogo.</p>
+    </div>
   </div>
 </template>
 
@@ -11,9 +24,62 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss">
-    div{
-      background-color: $color-dark;
-      background: linear-gradient(45deg, $color-dark 0%, $color-light 100%);
-    } 
+<style scoped lang="scss">
+
+  #banner{
+    background-color: $color-dark;
+    background: linear-gradient(45deg, $color-dark 0%, $color-light 100%);
+    height: 100vh;
+
+    #navbar{
+      padding: 2.5em 15em;
+      ul{
+        display: flex;
+        justify-content:space-around;
+        margin: 0;
+
+        li{
+          display: inline; 
+          color: $color-text-light;
+          font-size: 1.3em;
+          text-transform: lowercase;
+          font-weight: 300;
+          position: relative;
+
+          &:before{
+            transition: 400ms;
+            height: 2px;
+            content: "";
+            position: absolute;
+            top: 1.3em;
+            background-color: #031D44;
+            width: 0%;
+
+          }
+          &:hover{
+            cursor: pointer;
+            &::before{ width: 100%; }
+          }
+        }
+      }
+    }
+
+    #welcome{
+      position: absolute;
+      bottom: 0;
+      margin: 0 0 3em 3em;
+
+      p{
+        margin: 0;
+        font-size: 4em;
+        color: $color-text-light;
+        font-weight: 300;
+
+        &.name{
+          font-family: $font-text;
+          font-size: 3em;
+        }
+      }
+    }
+  }
 </style>
