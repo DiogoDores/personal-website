d92bf89368582b8cdd562f968030ee85b39c647a
Diogo Dores <diogo.dores@gmail.com>
2020-11-08 15:40:08 +0000

    Added configurations for SCSS

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Banner></Banner>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Banner from './components/Banner.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Banner
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: $font-title;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    yoyoyoyoyoy
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+    div{
+      background-color: $color-dark;
+      background: linear-gradient(45deg, $color-dark 0%, $color-light 100%);
+    } 
+</style>
